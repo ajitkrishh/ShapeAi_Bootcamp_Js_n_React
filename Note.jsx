4ae0633d3aee67ff64510f59a47ab27c500c4b05
ajitkrishh <57298585+ajitkrishh@users.noreply.github.com>
2021-06-06 10:04:37 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -6,7 +6,7 @@ export default function Note() {
       <h1> Javascript and React.js</h1>
       <p>
         This was an amazing bootcamp taken up by Shaurya Sinha. We covered
-        everything from scratch including javascript React.js HTML. Ii was
+        everything from scratch including javascript React.js HTML. It was
         really a nice bootcamp.
       </p>
     </div>
